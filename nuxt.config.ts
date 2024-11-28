5b46846b10340b705192498427bd8116eb02aa75
Zal Solmus <zalsolmus@gmail.com>
2024-11-28 13:09:27 +0300

    fix packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   typescript: {
-    typeCheck: true
+    typeCheck: false
   }
 })
